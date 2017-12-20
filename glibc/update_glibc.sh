26bf4585b57d8cf072ea1c5bcda40e6e0fe03a6d
XiaoMuYi <34268642+XiaoMuYi@users.noreply.github.com>
2017-12-20 11:54:21 +0800

    Update update_glibc.sh

--- glibc/update_glibc.sh
+++ glibc/update_glibc.sh
@@ -9,16 +9,16 @@ strings /lib64/libc.so.6 |grep GLIBC_
 
 yum install -y  curl openssh-server openssh-clients postfix cronie git nmap unzip wget lsof xz gcc make vim  curl gcc-c++ libtool
 
-cd /opt && wget http://ftp.gnu.org/gnu/glibc/glibc-2.24.tar.gz
+cd /opt 
+wget http://ftp.gnu.org/gnu/glibc/glibc-2.24.tar.gz
 
-tar zxvf glibc-2.24.tar.gz && cd glibc-2.24
-
+tar zxvf glibc-2.24.tar.gz 
+cd glibc-2.24
 mkdir build && cd build
+
 cp /etc/ld.so.conf /opt/glibc-2.24/etc/
-/opt/glibc-2.24/configure --prefix=/opt/glibc-2.24
-make
+../configure --prefix=/opt/glibc-2.24
+make -j4
 make install
 
-mkdir -p  /opt/glibc-2.24/etc
-touch /opt/glibc-2.24/etc/ld.so.conf
-export LD_LIBRARY_PATH=/opt/glibc-2.14/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/glibc-2.24/lib:$LD_LIBRARY_PATH
